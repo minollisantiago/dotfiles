6f5a1503d0f6f4d378857ae7c99a5a5d34f6c46f
Santiago Minolli <minollisantiago@gmail.com>
2024-07-05 15:04:38 -0300

    Updated powershell config with new fuzzy finder functions with aliases
    - ff: to run the fuzzy file finder with preview using bat
    - nff: to run the fuzzy file finder with preview, multiple file selection and opening on neovim

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -14,6 +14,15 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Import-Module PSFzf
 Set-PsFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl-r'
 
+#Fuzzy finder: open cat explorer mode with fuzzy finder (kinda like telescope)
+function Fuzz-Preview {
+  fzf --preview="bat --color=always {}"
+}
+
+function Fuzz-Preview-Nvim {
+  nvim $(fzf -m --preview="bat --color=always {}")
+}
+
 # Alias
 Set-Alias vim nvim
 Set-Alias cc clear
@@ -22,3 +31,5 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias ff Fuzz-Preview
+Set-Alias nff Fuzz-Preview-Nvim
