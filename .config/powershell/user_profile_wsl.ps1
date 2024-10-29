5c0948e27d0add67c5eea3ca20547191d637a2c5
Santiago Minolli <minollisantiago@gmail.com>
2024-10-29 11:30:40 -0300

    Added a powershell profile file for WSL

--- .config/powershell/user_profile_wsl.ps1
+++ .config/powershell/user_profile_wsl.ps1
@@ -1,7 +1,6 @@
 #OhMyPosh
 Import-Module posh-git
-Import-Module oh-my-posh
-Import-Module Terminal-Icons
+Import-Module Terminal-Icons -Force
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'santiago.omp.json'
 oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
@@ -22,18 +21,31 @@ function Fuzz-Preview-Nvim {
   nvim $(fzf -m --preview="bat --color=always {}")
 }
 
+# Display working directory contents
+function ll {
+    Get-ChildItem -Force
+}
+
 # Navigation shortcuts
 function .. { Set-Location .. }
 function ... { Set-Location ../.. }
 function .... { Set-Location ../../.. }
 
+# My workspace: location shortcuts example
+function my-custom-Workspace {
+  Set-Location /mnt/f/some/random/folder
+}
+
 # Alias
 Set-Alias vim nvim
 Set-Alias cc clear
-Set-Alias ll ls
+Set-Alias ls ll
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias ff Fuzz-Preview
 Set-Alias nff Fuzz-Preview-Nvim
+
+# Workspace alias
+Set-Alias ws1 my-custom-Workspace
