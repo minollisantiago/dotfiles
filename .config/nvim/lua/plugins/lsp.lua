8827d53555f32202654f016053f61287549112f5
Santiago Minolli <minollisantiago@gmail.com>
2024-10-31 12:28:47 -0300

    Updated lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,24 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
-        "html-lsp",
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"yaml-language-server",
+				"css-lsp",
+				"html-lsp",
+				"pyright",
+				"htmlhint",
+				"json-lsp",
+				"marksman",
+				"markdownlint",
+				"markdown-toc",
+				"markdownlint-cli2",
+				"stylua",
+				"taplo",
+				"sqlfluff",
+			})
+		end,
+	},
 }
