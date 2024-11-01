8d186809e78b320111eba272dafe4d731e3a86e5
Santiago Minolli <minollisantiago@gmail.com>
2024-11-01 13:24:27 -0300

    Updated colorschemes for nvim

--- .config/nvim/lua/plugins/colorscheme_cat.lua
+++ .config/nvim/lua/plugins/colorscheme_cat.lua
@@ -1,16 +1,19 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    require("catppuccin").setup({
-      transparent_background = true,
-      flavour = "macchiato",
-      integrations = {
-        mason = true,
-        neotree = true,
-      },
-    })
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = true,
+	priority = 1000,
+	opts = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+			flavour = "macchiato",
+			integrations = {
+				cmp = true,
+				mason = true,
+				neotree = true,
+				gitsigns = true,
+				treesitter = true,
+			},
+		})
+	end,
 }
